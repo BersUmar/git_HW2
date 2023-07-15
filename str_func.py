53d1dd36817ed8f0fb56b604d5b145f0248c7709
BersUmar <110242572+BersUmar@users.noreply.github.com>
2023-07-15 13:44:16 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,7 +1,2 @@
 def upper_words(word: str) -> str:
-    '''создаем функцию, которая позволит возвращать строку со всеми ее заглавными буквами'''
-    return word.upper()
-
-def title_words(word: str) -> str:
-    '''функция, возвращающая строку с заглавноц буквы'''
-    return word.title()
+   return word.upper()
